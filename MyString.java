b2a68739e686a9a4d1e465bb8fdf7c433bb030ee
ShaharMaaravi <shahar.maaravi@post.roni.ac.il>
2024-11-28 17:09:44 +0200

    Q3

--- MyString.java
+++ MyString.java
@@ -7,6 +7,7 @@ public class MyString {
         System.out.println("lowercase : " + lowerCase("lowercase"));
 
         System.out.println("Testing contains:");
+        System.out.println(contains("baba yaga", "baba"));
         System.out.println(contains("unhappy", "happy")); // true
         System.out.println(contains("happy", "unhappy")); // false
         System.out.println(contains("historical", "story")); // false
@@ -18,13 +19,30 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        String result = lowerCase(str);
+        String result = str.toLowerCase();
         return result;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+            if(str2.isEmpty()) return true;
+            char test = str2.charAt(0);
+
+            if(str1.indexOf(test) < 0) { 
+                return false; 
+            } else {
+                int i = str1.indexOf(test)+1;
+                int k = 1; 
+                while (k < str2.length()) {
+                    if(str1.charAt(i) != str2.charAt(k)){
+                        return false; 
+                    }
+                    i++; 
+                    k++;
+                }
+                return true;
+            }
+        
+       }
     }
-}
+
